6e16d68cae11b900e0fea5f282f92055422e7ed9
kainthashish <kainth.ashish@gmail.com>
2025-06-10 22:45:43 +0530

    fixed test case failing issue

--- src/components/__test__/Header.test.js
+++ src/components/__test__/Header.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Header from "../Header";
 import { Provider } from "react-redux";
-import appStore from "../../store";
+import appStore from "../../store/appStore";
 import { BrowserRouter } from "react-router-dom";
 
 it("Should load Header Component with a Login button", () => {
